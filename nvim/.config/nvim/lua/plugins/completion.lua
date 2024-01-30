62914367af08c7cbb1b8c8f7f13ba6c2585696d9
Jorge Alves <jorgeandrealves@gmail.com>
2024-01-30 09:57:55 +0000

    updates nvim

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -18,6 +18,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     lazy = false,
+    dependencies = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
     config = function()
       local cmp = require("cmp")
       cmp.setup({
@@ -42,6 +43,7 @@ return {
           { name = "luasnip" },
         }, {
           { name = "buffer" },
+          { name = "path" }
         }),
       })
     end,
