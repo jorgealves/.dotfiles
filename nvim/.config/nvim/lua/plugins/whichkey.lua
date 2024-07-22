4c2c8400cc587ad13871ad150ba10db3528c81b3
Jorge Alves <jorgeandrealves@gmail.com>
2024-07-22 11:01:54 +0100

    chore: add i3

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -1,16 +1,16 @@
-return {              -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-        require('which-key').setup()
-
-        -- Document existing key chains
-        require('which-key').register {
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-            ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-            ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        }
-    end,
+return { -- Useful plugin to show you pending keybinds.
+	-- "folke/which-key.nvim", event = "VeryLazy", -- Sets the loading event to 'VimEnter'
+	-- keys = {
+	-- 	{ "<leader>c", desc = "[C]ode" },
+	-- 	{ "<leader>d", desc = "[D]ocument" },
+	-- 	{ "<leader>r", desc = "[R]ename" },
+	-- 	{ "<leader>s", desc = "[S]earch" },
+	-- 	{ "<leader>w", desc = "[W]orkspace" },
+	-- },
+	-- config = function() -- This is the function that runs, AFTER loading
+	-- 	require("which-key").setup()
+	--
+	-- 	-- Document existing key chains
+	-- 	require("which-key").register({})
+	-- end,
 }
