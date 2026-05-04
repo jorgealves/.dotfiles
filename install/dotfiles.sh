7e558e493b538527ef51a24a1e70daf51008091f
Jorge Alves <6217708+jorgealves@users.noreply.github.com>
2026-05-04 09:20:27 +0100

    chore: configs

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -15,21 +15,34 @@ cd "$ROOT_DIR/config"
 if command -v zed &>/dev/null; then
   log_info "Configuring Zed editor 🧑‍💻"
   stow -R -v zed -t "$USER_HOME"
+else
+  stow -D -v zed -t "$USER_HOME"
+  log_info "Zed editor not found, skipping configuration 🧑‍💻"
 fi
 
 if command -v code &>/dev/null; then
   log_info "Configuring VS Code editor 🧑‍💻"
   stow -R -v vscode -t "$USER_HOME"
   jq -r '.recommendations[]' "$ROOT_DIR/config/vscode/Library/Application Support/Code/User/extensions.json" | xargs -n 1 code --install-extension
+else
+  stow -D -v code -t "$USER_HOME"
+  log_info "VS Code editor not found, skipping configuration 🧑‍💻"
 fi
 
 if command -v opencode &>/dev/null; then
   log_info "Configuring OpenCode AI 🧑‍💻"
   stow -R -v opencode -t "$USER_HOME"
+else
+  stow -D -v opencode -t "$USER_HOME"
+  log_info "OpenCode AI not found, skipping configuration 🧑‍💻"
 fi
+
 if command -v aerospace &>/dev/null; then
   log_info "Configuring Aerospace window manager 🧑‍💻"
   stow -R -v aerospace -t "$USER_HOME"
+else
+  stow -D -v aerospace -t "$USER_HOME"
+  log_info "Aerospace window manager not found, skipping configuration 🧑‍💻"
 fi
 
 log_footer "Dotfiles configuration installed successfully 🎉"
